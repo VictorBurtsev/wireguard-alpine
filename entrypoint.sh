53e11ba76b8315b81c875b0f4f3dcd200f472a3d
victor <victor.burtsev@gmail.com>
2023-03-25 02:58:37 -0400

    Init#3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-cat << EOF > /etc/wireguard/wg0.conf
-[Interface]
-PrivateKey = YP3dNo3cEbtPZThtZzkcve0Q74Nrz6ob5Y5sm1nLIXE=
-Address = 10.0.0.1/24
-ListenPort = 51820
-PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-
-[Peer]
-PublicKey = x4I5ZPyf9m0Pv7RduMCDBs6Amfleat1S3DcNmLOX8lc=
-AllowedIPs = 10.0.0.2/32
-
-[Peer]
-PublicKey = PHjiSlEEkz/yeHB/g7YjQjDRMRcTYaFDg8oflH5cdhA=
-AllowedIPs = 10.0.0.3/32
-EOF
-
 wg-quick up wg0
 
 while true
